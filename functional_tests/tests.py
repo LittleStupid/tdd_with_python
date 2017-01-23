eb5403be1fa8bb97eb96cda73426a0e43677100b
LittleStupid <lttlstpd@live.com>
2017-01-23 14:35:31 +0800

    refactor, test url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,10 +8,10 @@ from django.test import LiveServerTestCase
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
-        binary = FirefoxBinary(
+        self.binary = FirefoxBinary(
             '/Applications/Firefox.app/Contents/MacOS/firefox-bin')
-        self.browser = webdriver.Firefox(firefox_binary=binary)
-        self.browser.implicitly_wait(3)
+        self.browser = webdriver.Firefox(firefox_binary=self.binary)
+        # self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -22,11 +22,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        # print('-----')
-        # print(self.live_server_url)
-        # self.browser.get(self.live_server_url)
-
-        self.assertIn('To-Do', self.browser.title)
+        # self.assertIn('To-Do', self.browser.title)
 
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -38,19 +34,51 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+
+        # patch
+        k = 0
+        while k < 10000000:
+            k = k + 1
+
+        edith_list_url = self.browser.current_url
+
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        # patch
+        k = 0
+        while k < 10000000:
+            k = k + 1
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly')
 
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        # self.assertIn('2: Use peacock feathers to make a fly',
-        #               [row.text for row in rows])
+        self.browser.quit()
+        self.browser = webdriver.Firefox(firefox_binary=self.binary)
+
+        self.browser.get('http://localhost:8000')
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        # patch
+        k = 0
+        while k < 10000000:
+            k = k + 1
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
         self.fail('Finish the test!')
 
 # if __name__ == '__main__':
--- lists/views.py
+++ lists/views.py
@@ -6,7 +6,14 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        print('@@@@@@@@@@@@@@')
+        return redirect('/lists/the-only-list-in-the-world')
 
+    print('+++++')
+    return render(request, 'home.html')
+
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    print('##########')
+    return render(request, 'list.html', {'items': items})
